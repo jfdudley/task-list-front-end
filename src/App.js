eef8d5d7850346017d92e14cda270072d57621ba
Joanna Dudley <jfdudleybmc@gmail.com>
2022-06-23 14:49:28 -0400

    mostly passing wave 3

--- src/App.js
+++ src/App.js
@@ -1,43 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+const kBaseUrl = 'https://task-list-api-c17.herokuapp.com/tasks';
+
+const taskApiToJson = (task) => {
+  const { id, title, is_complete: isComplete, description } = task;
+  return { id, title, isComplete, description };
+};
+
+const getTasks = () => {
+  return axios
+    .get(kBaseUrl)
+    .then((response) => {
+      return response.data.map(taskApiToJson);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const changeComplete = (id, isComplete) => {
+  const completeStatus = isComplete ? 'mark_complete' : 'mark_incomplete';
+
+  return axios
+    .patch(`${kBaseUrl}/${id}/${completeStatus}`)
+    .then((response) => {
+      return taskApiToJson(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const removeTask = (id) => {
+  return axios.delete(`${kBaseUrl}/${id}`).catch((error) => {
+    console.log(error);
+  });
+};
 
 const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
-  const updateTaskData = (updatedTask) => {
-    console.log('updateTaskData');
-    const tasks = taskData.map((task) => {
-      if (task.id === updatedTask.id) {
-        return updatedTask;
-      } else return task;
+  const [taskData, setTaskData] = useState([]);
+
+  useEffect(() => {
+    getTasks().then((tasks) => {
+      setTaskData(tasks);
     });
-    setTaskData(tasks);
+  }, []);
+
+  const updateTaskData = (updatedTask) => {
+    changeComplete(updatedTask.id, updatedTask.isComplete).then(
+      setTaskData((oldData) => {
+        return oldData.map((task) => {
+          if (task.id === updatedTask.id) {
+            return updatedTask;
+          } else {
+            return task;
+          }
+        });
+      })
+    );
   };
+
   const deleteTask = (id) => {
-    console.log('deleteTask');
-    let tasks = [];
-    for (const task of taskData) {
-      if (task.id != id) {
-        tasks.push(task);
-      }
-    }
-    console.log(tasks);
-    setTaskData(tasks);
+    removeTask(id).then((task) => {
+      setTaskData((oldData) => {
+        return oldData.filter((task) => {
+          return task.id !== id;
+        });
+      });
+    });
   };
+
   return (
     <div className="App">
       <header className="App-header">
